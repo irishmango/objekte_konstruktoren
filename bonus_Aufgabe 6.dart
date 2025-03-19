2d3897e4d3831e9da531441d13acdf8c59b43485
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-19 12:53:01 +0100

    autor.name auf buch1.autor.name geändert

--- bonus_Aufgabe 6.dart
+++ bonus_Aufgabe 6.dart
@@ -21,6 +21,6 @@ void main() {
   Autor autor1 = Autor("J.R.R Tolkein", 1892);
   Buch buch1 = Buch("The Fellowship of the Ring", 432, autor1);
 
-  print("Title: ${buch1.titel} \nSeitenzahl: ${buch1.seitenzahl} \nAutor: ${autor1.name}");
+  print("Title: ${buch1.titel} \nSeitenzahl: ${buch1.seitenzahl} \nAutor: ${buch1.autor.name}");
 
 }
